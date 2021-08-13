8047ea37ac11fbd4a52437c075e9ec5c5e95d88f
Hydra <87888078+Hydravigguyt@users.noreply.github.com>
2021-08-13 20:15:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,9 +5,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("⚜️ My Group ⚜️", url="https://t.me/Tg_Hydra_Galaxy")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "⚜️ My Channel ⚜️", url="https://t.me/Tg_Hydra_Galaxy")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
