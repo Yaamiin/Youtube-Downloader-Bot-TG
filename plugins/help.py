5e69af6922db17fc5f639d08832b6e7ba17a28ee
Yaamiin <91520140+Yaamiin@users.noreply.github.com>
2021-11-22 17:16:20 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Hᴀᴅᴀ Rᴏʙᴏᴛᴋᴀɴ ᴡᴜxᴜ sᴏ ᴅᴀᴊɪɴ ᴋᴀʀᴀ Hᴀʟ Vɪᴅᴇᴏ ᴍᴀʀᴋɪɪsᴀ (Mᴀʀᴋᴀ Lɪɴᴋ ᴘʟᴀʏʟɪsᴛ ʜᴀ ɪsᴏ ᴅɪʀɪɴ) ᴋᴀʟɪʏᴀ ɪsᴏᴅɪʀ Yᴏᴜᴛᴜʙᴇ Vɪᴅᴇᴏ Lɪɴᴋ"
+    helptxt = f"Hᴀᴅᴀ Rᴏʙᴏᴛᴋᴀɴ ᴡᴜxᴜ sᴏ ᴅᴀᴊɪɴ ᴋᴀʀᴀ Hᴀʟ Vɪᴅᴇᴏ Mᴀʀᴋɪɪʙᴀ (Mᴀʀᴋᴀ Lɪɴᴋ ᴘʟᴀʏʟɪsᴛ ʜᴀ ɪsᴏ ᴅɪʀɪɴ) ᴋᴀʟɪʏᴀ ɪsᴏᴅɪʀ Yᴏᴜᴛᴜʙᴇ Vɪᴅᴇᴏ Lɪɴᴋ"
     await message.reply_text(helptxt)
